5c4916c33f2a6ba9f6ff2f37610399533e77c93d
KJAVA19811815 <karunsingh1993@gmail.com>
2017-04-03 04:40:53 +0000

    initial commit

---
+++ javascripting/if-statement.js
@@ -0,0 +1,8 @@
+var fruit = 'orange';
+var z = 'fruit.length';
+
+if (z>5) {
+  console.log('number of characters is greater than 5');
+} else {
+  console.log('The fruit name has more than five characters.');
+}
---
+++ javascripting/rounding-numbers.js
@@ -0,0 +1,4 @@
+var roundup = 1.5;
+var z = Math.round(1.5);
+var rounded = 'z';
+console.log(z);
